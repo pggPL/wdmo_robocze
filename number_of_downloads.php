ba9894952c076deda156691f0b7c31fe66da3621
pggPL <pawelgadzinski@gmail.com>
2022-07-09 18:51:57 +0200

    postgres migration

--- number_of_downloads.php
+++ number_of_downloads.php
@@ -6,7 +6,7 @@
 	$dbname = "serwer61148_wdmo";
 
 	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn = pg_connect("host=localhost:/tmp/postgres.sock dbname=wdmo user=wdmo password=1234");
 	// Check connection
 	if ($conn->connect_error) {
 	  die("Connection failed: " . $conn->connect_error);
@@ -17,7 +17,7 @@
 	$result = $conn->query($sql);
 
 
-	$conn->close();
+	pg_close($conn);
 
 
 
