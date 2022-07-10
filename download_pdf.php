cd3e18c4749fd1330502a31e65ebfb549560ab7c
pggPL <pawelgadzinski@gmail.com>
2022-07-10 12:28:28 +0200

    postgres migration

--- download_pdf.php
+++ download_pdf.php
@@ -1,31 +1,25 @@
 <?php
-	$servername = "sql48.lh.pl";
-	$username = "serwer61148_wdmo";
-	$password = "KTK2009ktk";
-	$dbname = "serwer61148_wdmo";
-
 	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn = pg_connect("host=/var/run/postgresql dbname=wdmo user=wdmo password=1234");
 	// Check connection
-	if ($conn->connect_error) {
+	if (!$conn) {
 	  die("Connection failed: " . $conn->connect_error);
 	  header("Location: https://github.com/pggPL/wdmo/raw/main/main.pdf");
 	} 
 
 	$sql = "INSERT INTO log (ip, time, type) VALUES ('".$_SERVER['REMOTE_ADDR']."', '".date("Y-m-d h:i:sa")."', 'pdf')";
-	$conn->query($sql);
+	pg_exec($conn, $sql);
 
 
 	$sql = "SELECT IP FROM IPs WHERE IP = '".$_SERVER['REMOTE_ADDR']."'";
-	$result = $conn->query($sql);
+	$result = pg_exec($conn, $sql);
 
-	if($result->num_rows == 0){
+	if(pg_num_rows($result) == 0){
 		$sql = "INSERT INTO IPs (IP) VALUES ('".$_SERVER['REMOTE_ADDR']."')";
-		$conn->query($sql);
+		$result = pg_exec($conn, $sql);
 	}
 
-	$conn->close();
-
+	pg_close($conn);
 
 
 	header("Location: https://github.com/pggPL/wdmo/raw/main/main.pdf")
--- read_online.php
+++ read_online.php
@@ -1,32 +1,27 @@
 <?php
-	$servername = "sql48.lh.pl";
-	$username = "serwer61148_wdmo";
-	$password = "KTK2009ktk";
-	$dbname = "serwer61148_wdmo";
-
 	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn = pg_connect("host=/var/run/postgresql dbname=wdmo user=wdmo password=1234");
 	// Check connection
-	if ($conn->connect_error) {
+	if (!$conn) {
 	  die("Connection failed: " . $conn->connect_error);
 	  header("Location: contents.html");
 	} 
 
-	$sql = "INSERT INTO log (ip, time, type) VALUES ('".$_SERVER['REMOTE_ADDR']."', '".date("Y-m-d h:i:sa")."', 'online')";
-	$conn->query($sql);
+	$sql = "INSERT INTO log (ip, time, type) VALUES ('".$_SERVER['REMOTE_ADDR']."', '".date("Y-m-d h:i:sa")."', 'pdf')";
+	pg_exec($conn, $sql);
 
 
 	$sql = "SELECT IP FROM IPs WHERE IP = '".$_SERVER['REMOTE_ADDR']."'";
-	$result = $conn->query($sql);
+	$result = pg_exec($conn, $sql);
 
-	if($result->num_rows == 0){
+	if(pg_num_rows($result) == 0){
 		$sql = "INSERT INTO IPs (IP) VALUES ('".$_SERVER['REMOTE_ADDR']."')";
-		$conn->query($sql);
+		$result = pg_exec($conn, $sql);
 	}
 
-	$conn->close();
+	pg_close($conn);
 
 
 
-	header("Location: contents.html")
+	header("Location: contents.html");
 ?>
